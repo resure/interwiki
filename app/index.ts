44f6db7fedbd25c162c0d1a956033af2ccd149bd
Gadzhi Gadzhiev <resure@zireael.org>
2019-04-06 01:49:29 +0300

    add wiki, lang and page to logs as separate columns

--- app/index.ts
+++ app/index.ts
@@ -28,6 +28,9 @@ module.exports = (req: IncomingMessage, res: ServerResponse) => {
     type: 'request',
     addr: ip as string,
     client,
+    s1: wiki,
+    s2: lang,
+    s3: page
   });
 
   return renderTemplate(wiki, page, lang);
